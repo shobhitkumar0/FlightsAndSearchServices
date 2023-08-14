57cca9be20d3f2c4e98246370c8ef3c9efcd51f9
Shobhit <souryashobhit206@gmail.com>
2023-08-15 00:14:15 +0530

    added -airplanes

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,5 +2,6 @@ const dotenv=require(`dotenv`);
 dotenv.config();
 module.exports={
     // create an express object
-    PORT:process.env.PORT
+    PORT:process.env.PORT||3010
+
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const {PORT} =require('./config/serverConfig');
 const ApiRoutes=require('./routes/index');
 
 const db =require('./models/index');
-const {City,Airport} =require('./models/index');
+const {Airplane} =require('./models/index');
 
 const setupAndStartServer= async()=>{
     //create and express object
@@ -13,21 +13,18 @@ const setupAndStartServer= async()=>{
    
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
-
+   
     app.use('/api',ApiRoutes);
     app.listen(PORT,async()=>{
         console.log(`server started at ${PORT}`);
         if(process.env.SYNC_DB){
             db.sequelize.sync({alter:true});
         }
-        //
-        // const city =await City.findOne({
-        //     where:{
-        //         id:352
-        //     }
-        // })
-        // const airports=await city.getAirports();
-        // console.log(city,airports);
+        await Airplane.create({
+            modelNumber:'Bombardier CRJ2',
+            capacity:200
+        });
+    
 
     });
 
