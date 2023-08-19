9680bee35be272d9e4a623b778c585971b483e26
Shobhit <souryashobhit206@gmail.com>
2023-08-20 02:44:18 +0530

    added common crud classees for basic crud operations

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const CityController=require('../../controllers/city-controller');
 const FlightController =require('../../controllers/flight-controller');
+const AirportController=require('../../controllers/airport-controller')
 
 const router =express.Router();
 
@@ -9,6 +10,10 @@ router.delete('/city/:id',CityController.destroy);
 router.get('/city/:id',CityController.get);
 router.patch('/city/:id',CityController.update);
 router.get('/city',CityController.getAll);
+
 router.post('/flight',FlightController.create);
 router.get('/flight',FlightController.getAll);
+
+
+router.post('/airports',AirportController.create);
 module.exports=router;
